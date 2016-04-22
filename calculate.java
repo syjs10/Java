e7fa4340666caaffbd33ae1416f9c23ff099e7e1
syjs10 <350172898@qq.com>
2016-04-22 23:14:50 +0800

    java

---
+++ calculate.java
@@ -0,0 +1,8 @@
+public class calculate {
+    public static void main(String[] args) {
+        int i1 = 5, i2 = 2;
+        System.out.println((i1 + i2) + " " + (i1 - i2));
+        System.out.println((i1 * i2) + " " + (i1 / i2));
+        System.out.println(i1 % i2);
+    }
+}
